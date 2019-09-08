3a89f7af9cef1f39de113fca0a77fd168500e103
Khusharth <khusharth19@gmail.com>
2019-09-08 16:08:26 +0530

    Added ImageLinkForm And Rank Component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className="f4 link grow black pa3 pointer bg-purple br-pill mr4 white"> Sign Out</p>
+            <p className="f5 link grow black pa3 pointer bg-purple br-pill mr4 white"> Sign Out</p>
         </nav>
     );
 }
