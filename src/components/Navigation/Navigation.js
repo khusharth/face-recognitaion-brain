7267bfd3418e0c9879de6e4459165a95974d8097
Khusharth <khusharth19@gmail.com>
2019-09-08 13:56:07 +0530

    Added Navigation Component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className="f4 link grow black pa3 pointer bg-purple br-pill mr4 white"> Sign Out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
