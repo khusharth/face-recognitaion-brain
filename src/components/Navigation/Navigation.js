8579777bd617cdce2709bec0cdf86293e5ddc52e
Khusharth <khusharth19@gmail.com>
2019-09-12 10:43:08 +0530

    Added conditional routing for navigation menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Navigation = () => {
-    return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className="f5 link grow black pa3 pointer bg-purple br-pill mr4 white"> Sign Out</p>
-        </nav>
-    );
+const Navigation = ( {onRouteChange, isSignedIn} ) => {
+    if (isSignedIn) {
+        return (
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <p onClick={() => onRouteChange('signout')} className="f5 link grow black pa3 pointer bg-purple br-pill mr4 white"> Sign Out</p>
+            </nav>
+        );
+    } else {
+        return (
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <p onClick={() => onRouteChange('signin')} className="f5 link grow black pa3 pointer bg-purple br-pill mr4 white"> Sign in</p>
+                <p onClick={() => onRouteChange('register')} className="f5 link grow black pa3 pointer bg-purple br-pill mr4 white"> Register</p>                
+            </nav>
+        );
+    }
+    
 }
 
 export default Navigation;
